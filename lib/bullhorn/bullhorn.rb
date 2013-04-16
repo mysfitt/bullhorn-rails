5cad822be07e98573812ced9bd43bb7ef8f4a629
Joseph Reeves <joseph.reeves@gmail.com>
2013-04-16 13:38:58 -0400

    added check for development environment with reduced logging for production.

--- lib/bullhorn/bullhorn.rb
+++ lib/bullhorn/bullhorn.rb
@@ -14,18 +14,20 @@ require 'bullhorn/api'
 module Bullhorn
 
   Savon.configure do |config|
+    if defined?(Rails) and Rails.env == "development"
+      # log at debug level only in development.
+      # The default log level used by Savon is :debug.
+      config.log_level = :debug
+      # In a Rails application you might want Savon to use the Rails logger.
+      #config.logger = Rails.logger
 
-    # TODO Add logger based on environment
-    # The default log level used by Savon is :debug.
-    config.log_level = :debug
-
-    # In a Rails application you might want Savon to use the Rails logger.
-    #config.logger = Rails.logger
-
-    # The XML logged by Savon can be formatted for debugging purposes.
-    # Unfortunately, this feature comes with a performance and is not
-    # recommended for production environments.
-    config.pretty_print_xml = true
-
+      # The XML logged by Savon can be formatted for debugging purposes.
+      # Unfortunately, this feature comes with a performance and is not
+      # recommended for production environments.
+      config.pretty_print_xml = true
+    else
+      # be quiet(ish) in production
+      config.log_level = :info
+    end
   end
 end
