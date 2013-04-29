03e1ed3489bc7d97159f2437163f6b61a50c8be5
Joseph Reeves <joseph.reeves@gmail.com>
2013-04-29 17:11:53 -0400

    refactored some of the job submission stuff to candidates

--- lib/bullhorn/jobsubmission.rb
+++ lib/bullhorn/jobsubmission.rb
@@ -39,7 +39,7 @@ module Bullhorn
         response = Bullhorn::Candidates.query_by email
         unless !response.nil? && response.has_key?(:user_id)
           new_candidate = Bullhorn::Candidates.default_candidate
-          response = create_candidate(candidate_create_request(new_candidate, email))
+          response = create_candidate(Bullhorn::Candidates.candidate_create_request(new_candidate, email))
         end
         response[:user_id]
       end
@@ -91,79 +91,7 @@ module Bullhorn
           }
         end
 
-        def candidate_create_request dto, email
-          candidate =
-          {
-            :address => {
-              :address1 => @address1,
-              :address2 => @address2,
-              :city => @city,
-              :state => @state,
-              :countryID => 1,
-              :zip => @zip
-              },
-            :categoryID => 45,
-            :comments => "",   
-            :email => @email,
-            :phone => @phone,                         
-            :employeeType => "W2",
-            :firstName => @firstname,
-            :isDeleted => false,
-            :isEditable => true,
-            :lastName => @lastname,
-            :name => "#{@firstname} #{@lastname}",
-            :ownerID => 43832,
-            :password => "hireminds123",
-            :preferredContact => "Email",
-            :status => "New Lead",
-            :username => @email,
-            :userTypeID => 35,  
-            :isDayLightSavings => true,
-            :massMailOptOut => false,
-            :timeZoneOffsetEST => 0,
-            :dayRate => 0.0,
-            :dayRateLow => 0.0,
-            :degreeList => "",
-            :salary => @salary,
-            :travelLimit => 0,
-            :willRelocate => false,
-            :workAuthorized => true,
-            :attributes! => { 
-              :categoryID => {"xsi:type" => "xsd:int"},
-              :ownerID => {"xsi:type" => "xsd:int"},
-              :isDeleted => {"xsi:type" => "xs:boolean"},
-              :isEditable => {"xsi:type" => "xs:boolean"},
-              :isDayLightSavings => {"xsi:type" => "xs:boolean"},
-              :massMailOptOut => {"xsi:type" => "xs:boolean"},
-              :userTypeID => {"xsi:type" => "xsd:int"},
-              :timeZoneOffsetEST => {"xsi:type" => "xsd:int"},
-              :dayRate => {"xsi:type" => "xsd:decimal"},
-              :dayRateLow => {"xsi:type" => "xsd:decimal"},
-              :salary => {"xsi:type" => "xsd:decimal"},
-              :travelLimit => {"xsi:type" => "xsd:int"},
-              :willRelocate => {"xsi:type" => "xs:boolean"},
-              :workAuthorized => {"xsi:type" => "xs:boolean"},
-            }
-          }
-
-          # Snakecase for the merge
-          candidate = Hash[candidate.map {|k,v| [k.to_s.snakecase.to_sym, v]}]
-          candidate = dto.merge(candidate)
-
-          if candidate.has_key?(:user_id)
-            candidate[:userID] = candidate[:user_id]
-            candidate[:attributes!][:userID] = {"xsi:type" => "xs:int"}
-          else
-            candidate[:username] = email
-            candidate[:password] = "hireminds766"
-          end 
-          [:"@xmlns:xsi", :"@xmlns:ns4", :"@xsi:type", :alerts].each do |key|
-            candidate.delete(key)
-          end
-
-          # Camelcase for the SOAP request. Upcase Id to ID
-          candidate = Hash[candidate.map {|k,v| [k.to_s.camelize.sub(/Id/, "ID").to_sym, v]}]
-        end
+        
 
     end
   end
