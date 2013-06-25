54741942a1f7eeaeb7b50e54736858bf027fcae2
Ben Lambert <ben@bowst.com>
2013-06-25 14:56:34 -0400

    fix filters

--- lib/bullhorn/jobs.rb
+++ lib/bullhorn/jobs.rb
@@ -55,7 +55,7 @@ module Bullhorn
       protected
       
       def all_jobs_query
-        { :query => { :entityName => "JobOrder", :where => "dateAdded >= #{1.year.ago..strftime("%Y-%m-%d %H:%M")" } }
+        { :query => { :entityName => "JobOrder", :where => "dateAdded >= #{1.year.ago..strftime("%Y-%m-%d")" } }
       end
 
       def open_approved_job_id_request 
